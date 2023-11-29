80196d4e955b634113d48d1b951a3eb72291f26b
UriShilo <urishilo360@gmail.com>
2023-11-29 15:04:06 +0200

    Add renderMeme()

--- js/meme-generator.controller.js
+++ js/meme-generator.controller.js
@@ -1,1 +1,18 @@
 'usr strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+
+}
+
+function renderMeme(elImg) {
+  const elTitle = document.querySelector('.meme-title')
+  elTitle.innerText = prompt('Enter new meme title:')
+
+  gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+  gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
